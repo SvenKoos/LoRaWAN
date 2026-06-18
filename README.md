94fd064ca96e3d0b6453d3eeeea7ec9fecbcfb86
SvenKoos <sven@koosens.ch>
2026-06-18 02:25:03 +0200

    Sensor connected
    - Changed code base to LoRa P2P
    - LoRa receiver removed
    - Sensor connectivity added

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # LoRaWAN
-LoRaWAN implementation with temperature and humidity sensor / display based on LilyGo T-Echo, LoRaWAN gateway RAK7246G and TTN
+LoRaWAN implementation with temperature and humidity sensor / display based on LilyGo T-Echo, LoRaWAN gateway RAK7246G / RAK5146 and TTN
 
 ## Project plan
 1. connect temperature / humidity sensor SHT31 to T-Echo Lite
@@ -8,7 +8,7 @@ LoRaWAN implementation with temperature and humidity sensor / display based on L
 4. create application in TTN and get the TTN keys
 5. extend the code on T-Echo Lite about LoRaWAN implementation (sensor authentication, measurement upload)
 
-## Connect sensor to T-Echo
+## 1. Connect sensor to T-Echo
 - connect SHT31 to T-Echo Lite using the following pins (to be verified)
 
 | Sensor pin	| T-Echo pin	| Arduino pin
@@ -18,47 +18,45 @@ LoRaWAN implementation with temperature and humidity sensor / display based on L
 | SDA			| P1.04 (PI.04) | 36
 | SCL			| P1.03 (PI.03) | 35
 
-## Show measurements on T-Echo
+## 2. Show measurements on T-Echo
 
 
-## Configure gateway
+## 3. Configure gateway
 - configure gateway network, internet access, LoRa band, TTN network
- - RAK7246G:
+### RAK7246G:
   - [RAK7246G Quick Start Guide](https://docs.rakwireless.com/product-categories/wisgate/rak7246g/quickstart)
   - verify / change the TTN server name to new TTN V3 name (assuming EU1 selected in gateway setup in TTN): eu1.cloud.thethings.network
   - enable and restart LoRa services (assuming service name ttn-gateway): sudo systemctl enable ttn-gateway | sudo systemctl start ttn-gateway | sudo systemctl status ttn-gateway
   - verify communication of the gateway with TTN: sudo journalctl -u ttn-gateway -f 
- - RAK5146 PiHAT Kit for LoRaWAN and Concentrator (SPI model)
+### RAK5146 PiHAT Kit for LoRaWAN and Concentrator (SPI model)
   - [1. RAK5146 Setup with LoRa Basic Station](https://lora.vsb.cz/index.php/433-868-mhz-rak5146l-rak5146-lora-basics-station/)
   - [2. Setting Up a LoRa Gateway with Raspberry Pi and RAK5146](https://medium.com/@techworldthink/setting-up-a-lora-gateway-with-raspberry-pi-and-rak5146-f1af49a84ff7)
+  - [3. RAK Common for Gateway](https://github.com/RAKWireless/rak_common_for_gateway)
+  - [4. RAK2287 Quick Start Guide](https://docs.rakwireless.com/product-categories/wislink/rak2287/quickstart/)
   - Raspberry Pi OS setup acc. 1.
   - Remote connection via SSH acc. 1.
   - Post-installation steps (Update, upgrade and reboot Raspberry Pi) acc. 1
   - Setup Raspberry Pi’s Interfaces (SPI, I2C) acc. 1.
-  - Install RAK Common for Gateway acc. 2.
-  - Configure the Gateway for TTN acc. 2.
+  - Install RAK Common for Gateway acc. 2. and 3.
+  - Configure the Gateway for TTN acc. 2. and 4.
+  - verify / change connection to TTN server as for RAK7246G
 
-## Register in TTN
+## 4. Register in TTN
 - register the gateway with TTN (outdated, still helpful)
-
-[RAK7246G LoRaWAN Network Server Guide](https://docs.rakwireless.com/product-categories/wisgate/rak7246g/lorawan-network-server-guide)
+  - [RAK7246G LoRaWAN Network Server Guide](https://docs.rakwireless.com/product-categories/wisgate/rak7246g/lorawan-network-server-guide)
 
 - create organization and application in TTN (outdated, still helpful)
-
-[Setup LoRaWAN Network in TTN](https://docs.rakwireless.com/product-categories/wisgate/rak7246g/lorawan-network-server-guide)
+  - [Setup LoRaWAN Network in TTN](https://docs.rakwireless.com/product-categories/wisgate/rak7246g/lorawan-network-server-guide)
 
 - register gateway in TTNS
+  - [Setup LoRaWAN gateway on TTN Gateway Pro](https://www.thethingsindustries.com/docs/getting-started/3-try-starter-kit/)
 
-[Setup LoRaWAN gateway on TTN Gateway Pro](https://www.thethingsindustries.com/docs/getting-started/3-try-starter-kit/)
-
-## Get TTN application keys
+## 5. Get TTN application keys
 - register application with TTN (outdated)
+  - [RAK7246G LoRaWAN Network Server Guide](https://docs.rakwireless.com/product-categories/wisgate/rak7246g/lorawan-network-server-guide)
 
-[RAK7246G LoRaWAN Network Server Guide](https://docs.rakwireless.com/product-categories/wisgate/rak7246g/lorawan-network-server-guide)
-
-
-## Send measurements to TTN
+## 6. Send measurements to TTN
 - use RadioLib in LoRaWAN mode
- - implement OTAA (over the air activation) of the sensor / T-Echo
- - create the LoRaWAN payload for measurement data with CayenneLPP
+  - implement OTAA (over the air activation) of the sensor / T-Echo
+  - create the LoRaWAN payload for measurement data with CayenneLPP
 - use CayenneLPP library to create LoRaWAN paylod
---
+++ T-EchoLite/T-EchoLite-WAN.ino
@@ -0,0 +1,304 @@
+#include <Arduino.h>
+#include "Adafruit_EPD.h"
+#include "RadioLib.h"
+#include "t_echo_lite_config.h"
+#include "Display_Fonts.h"
+#include "Adafruit_SHT31.h"  // Bibliothek hinzufügen
+
+static const uint32_t Local_MAC[2] = {
+  NRF_FICR->DEVICEID[0],
+  NRF_FICR->DEVICEID[1],
+};
+
+struct SX1262_Operator {
+  using mode = enum {
+    LORA,  // lora mode
+    FSK,   // fsk mode
+  };
+
+  // aligned with T-Watch
+  struct
+  {
+    float value = 868.1;
+    bool change_flag = false;
+  } frequency;
+  struct
+  {
+    float value = 125.0;
+    bool change_flag = false;
+  } bandwidth;
+  struct
+  {
+    // uint8_t value = 12;
+    uint8_t value = 9;
+    bool change_flag = false;
+  } spreading_factor;
+  struct
+  {
+    // uint8_t value = 8;
+    uint8_t value = 7;
+    bool change_flag = false;
+  } coding_rate;
+  struct
+  {
+    // uint8_t value = 0xAB;
+    uint8_t value = 0x12;
+    bool change_flag = false;
+  } sync_word;
+  struct
+  {
+    // int8_t value = 22;
+    int8_t value = 13;
+    bool change_flag = false;
+  } output_power;
+  struct
+  {
+    // float value = 140;
+    float value = 60;
+    bool change_flag = false;
+  } current_limit;
+  struct
+  {
+    // int16_t value = 16;
+    int16_t value = 8;
+    bool change_flag = false;
+  } preamble_length;
+  struct
+  {
+    bool value = false;
+    bool change_flag = false;
+  } crc;
+
+  uint8_t current_mode = mode::LORA;
+
+  bool initialization_flag = false;
+};
+
+SX1262_Operator SX1262_OP;
+
+SPIClass Custom_SPI_0(NRF_SPIM0, SCREEN_MISO, SCREEN_SCLK, SCREEN_MOSI);
+Adafruit_SSD1681 display(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_DC, SCREEN_RST,
+                         SCREEN_CS, SCREEN_SRAM_CS, SCREEN_BUSY, &Custom_SPI_0);
+
+SPIClass Custom_SPI_3(NRF_SPIM3, SX1262_MISO, SX1262_SCLK, SX1262_MOSI);
+SX1262 radio = new Module(SX1262_CS, SX1262_DIO1, SX1262_RST, SX1262_BUSY, Custom_SPI_3);
+
+Adafruit_SHT31 sht31 = Adafruit_SHT31();  // Globales Objekt
+
+void Set_SX1262_RF_Transmitter_Switch(bool status) {
+  if (status == true) {
+    digitalWrite(SX1262_RF_VC1, HIGH);  // send
+    digitalWrite(SX1262_RF_VC2, LOW);
+  } else {
+    digitalWrite(SX1262_RF_VC1, LOW);  // receive
+    digitalWrite(SX1262_RF_VC2, HIGH);
+  }
+}
+
+bool SX1262_Set_Default_Parameters(String *assertion) {
+  if (radio.setFrequency(SX1262_OP.frequency.value) != RADIOLIB_ERR_NONE) {
+    *assertion = "Failed to set frequency value";
+    return false;
+  }
+  if (radio.setBandwidth(SX1262_OP.bandwidth.value) != RADIOLIB_ERR_NONE) {
+    *assertion = "Failed to set bandwidth value";
+    return false;
+  }
+  if (radio.setOutputPower(SX1262_OP.output_power.value) != RADIOLIB_ERR_NONE) {
+    *assertion = "Failed to set output_power value";
+    return false;
+  }
+  if (radio.setCurrentLimit(SX1262_OP.current_limit.value) != RADIOLIB_ERR_NONE) {
+    *assertion = "Failed to set current_limit value";
+    return false;
+  }
+  if (radio.setPreambleLength(SX1262_OP.preamble_length.value) != RADIOLIB_ERR_NONE) {
+    *assertion = "Failed to set preamble_length value";
+    return false;
+  }
+  if (radio.setCRC(SX1262_OP.crc.value) != RADIOLIB_ERR_NONE) {
+    *assertion = "Failed to set crc value";
+    return false;
+  }
+  if (SX1262_OP.current_mode == SX1262_OP.mode::LORA) {
+    if (radio.setSpreadingFactor(SX1262_OP.spreading_factor.value) != RADIOLIB_ERR_NONE) {
+      *assertion = "Failed to set spreading_factor value";
+      return false;
+    }
+    if (radio.setCodingRate(SX1262_OP.coding_rate.value) != RADIOLIB_ERR_NONE) {
+      *assertion = "Failed to set coding_rate value";
+      return false;
+    }
+    if (radio.setSyncWord(SX1262_OP.sync_word.value) != RADIOLIB_ERR_NONE) {
+      *assertion = "Failed to set sync_word value";
+      return false;
+    }
+  } else {
+  }
+  return true;
+}
+
+void GFX_Print_SX1262_Info(void) {
+  display.fillScreen(EPD_WHITE);
+
+  display.setFont(&FreeMonoBold9pt7b);
+  display.setCursor(5, 20);
+  display.setTextSize(1);
+
+  display.printf("SX1262 Info");
+
+  display.setFont(&FreeSans9pt7b);
+
+  display.setCursor(5, 40);
+  display.printf("MAC 0: %u", Local_MAC[0]);
+  display.setCursor(5, 60);
+  display.printf("MAC 1: %u", Local_MAC[1]);
+}
+
+void GFX_Print_SX1262_Init_Successful_Refresh_Info(void) {
+  display.setTextSize(1);
+  display.setCursor(5, 80);
+  display.printf("Status: Init successful");
+
+  display.setCursor(5, 100);
+  if (SX1262_OP.current_mode == SX1262_OP.mode::LORA) {
+    display.printf("Mode: LoRa");
+  } else {
+    display.printf("Mode: FSK");
+  }
+
+  display.setCursor(5, 120);
+  display.printf("Frequency: %.1f MHz", SX1262_OP.frequency.value);
+
+  display.setCursor(5, 140);
+  display.printf("Bandwidth: %.1f KHz", SX1262_OP.bandwidth.value);
+
+  display.setCursor(5, 160);
+  display.printf("Output Power: %d dBm", SX1262_OP.output_power.value);
+}
+
+void GFX_Print_SX1262_Init_Failed_Refresh_Info(void) {
+  display.setTextSize(1);
+  display.setCursor(5, 80);
+  display.printf("Status: Init failed");
+}
+
+bool SX1262_Initialization(void) {
+  Custom_SPI_3.begin();
+  Custom_SPI_3.setClockDivider(SPI_CLOCK_DIV2);
+
+  int16_t state = -1;
+  if (SX1262_OP.current_mode == SX1262_OP.mode::LORA) {
+    state = radio.begin();
+  } else {
+    state = radio.beginFSK();
+  }
+
+  if (state == RADIOLIB_ERR_NONE) {
+    String temp_str;
+    if (SX1262_Set_Default_Parameters(&temp_str) == false) {
+      Serial.printf("SX1262 Failed to set default parameters\n");
+      Serial.printf("SX1262 assertion: %s\n", temp_str.c_str());
+      return false;
+    }
+    if (radio.startReceive() != RADIOLIB_ERR_NONE) {
+      Serial.printf("SX1262 Failed to start receive\n");
+      return false;
+    }
+  } else {
+    Serial.printf("SX1262 initialization failed\n");
+    Serial.printf("Error code: %d\n", state);
+    return false;
+  }
+
+  Serial.printf("SX1262 initialization successful\n");
+
+  return true;
+}
+
+void setup(void) {
+  Serial.begin(115200);
+
+  // 3.3V Power ON
+  pinMode(RT9080_EN, OUTPUT);
+  digitalWrite(RT9080_EN, HIGH);
+
+  pinMode(SCREEN_BS1, OUTPUT);
+  digitalWrite(SCREEN_BS1, LOW);
+
+  pinMode(SX1262_RF_VC1, OUTPUT);
+  pinMode(SX1262_RF_VC2, OUTPUT);
+
+  // receive
+  Set_SX1262_RF_Transmitter_Switch(false);
+
+  pinMode(nRF52840_BOOT, INPUT_PULLUP);
+  pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
+  digitalWrite(LED_1, HIGH);
+  digitalWrite(LED_2, HIGH);
+
+  display.begin();
+  display.setRotation(1);
+  display.setTextColor(EPD_BLACK);
+
+  GFX_Print_SX1262_Info();
+  if (SX1262_Initialization() == true) {
+    GFX_Print_SX1262_Init_Successful_Refresh_Info();
+    SX1262_OP.initialization_flag = true;
+  } else {
+    GFX_Print_SX1262_Init_Failed_Refresh_Info();
+    SX1262_OP.initialization_flag = false;
+  }
+  display.display();
+
+  // 2. I2C-Pins festlegen UND den Bus starten
+  delay(100);
+  Wire.setPins(35, 36);
+  Wire.begin();
+  delay(100);
+
+  // 3. Sensor initialisieren
+  if (!sht31.begin(0x44)) {
+    Serial.println("SHT3x nicht gefunden!");
+  } else {
+    Serial.println("SHT3x bereit.");
+  }
+}
+
+void loop() {
+  // 2. Sensor-Messung (alle 60 Sekunden)
+  static unsigned long lastSensorMillis = -60000;
+  unsigned long currentMillis = millis();
+  if (currentMillis - lastSensorMillis >= 60000) {
+    lastSensorMillis = currentMillis;
+
+    // 1. I2C Bus aktiv für die Messung vorbereiten
+    Wire.begin();
+
+    float t = sht31.readTemperature();
+    float h = sht31.readHumidity();
+
+    // 2. I2C Bus sofort wieder "hart" beenden/freigeben
+    // Das verhindert, dass die I2C-Hardware auf den Pins hängen bleibt
+    Wire.end();
+
+    if (!isnan(t)) {
+      // 3. SPI-Bus für das Display exklusiv zurückgewinnen
+      Custom_SPI_0.end();
+      Custom_SPI_0.begin();
+
+      display.clearBuffer();
+      display.setFont(&FreeSans9pt7b);
+      display.setTextSize(3);
+      display.setCursor(5, 50);
+      display.printf("%.1f C", t);
+      display.setCursor(5, 120);
+      display.printf("%.1f %%", h);
+      display.display();
+
+      // SPI nach Display-Refresh wieder schlafen legen, um LoRa nicht zu stören
+      Custom_SPI_0.end();
+    }
+  }
+}
